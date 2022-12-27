34ce6f18d77d247e8b9e10198fc0c99847a81ee0
Ryan Walker <razor222ftw@gmail.com>
2022-12-27 22:00:20 +0800

    gameSearching

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser")
+const crypto = require("crypto")
 const session = require("express-session");
 const app = express();
 require("dotenv").config();
@@ -11,6 +13,8 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(bodyParser.urlencoded({ extended: true })); //add this
+app.use(bodyParser.json());
 app.use(sessionChecker);
 app.use(urlLogger)
 app.use(express.static('views'))
@@ -29,13 +33,16 @@ app.use("/game", gameRouter);
 
 function sessionChecker(req, res, next) {
   if (req.originalUrl.startsWith("/user/new/") || req.originalUrl == "/") return next();
-  if (!req.session.username /*|| !req.session.uuid*/) return res.redirect("/");
+  if (req.method != "POST" && (!req.session.username /*|| !req.session.uuid*/)) return res.redirect("/");
   next();
   // We aren't checking for missing a uuid as it isn't implemented.
 }
 
 function urlLogger(req, res, next) {
-    console.log(req.originalUrl)
+    ignoreUrls = [
+      "/game.css"
+    ]
+    if (!ignoreUrls.includes(req.originalUrl)) console.log(req.originalUrl)
     next();
 }
 
--- routes/user.js
+++ routes/user.js
@@ -1,23 +1,28 @@
 const express = require("express");
 const router = express.Router();
 
+const usersArray = []; // Users will be added to the database later down the line.
+
 router.get("/new/:username", (req, res) => {
   res.redirect("/game/");
+
 });
 
 router.get("/logout", (req, res) => {
   
 })
 
-const users = []; // Users will be added to the database later down the line.
+router.get("/list", (req, res) => {
+  res.status(200).send(usersArray);
+});
 router.param("username", (req, res, next, username) => {
-  //Check for already take Username
   req.session.username = username;
   req.session.uuid = crypto.randomUUID();
-  users[username] = {
+  const user = {
     username: req.session.username,
     uuid: req.session.uuid,
-  };
+  }
+  usersArray.push(user)
   next();
 });
 
