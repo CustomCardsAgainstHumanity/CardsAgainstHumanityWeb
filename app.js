b399a3b9911f65dad4d58ce7d1725801a1d0438f
Ryan Walker <razor222ftw@gmail.com>
2022-12-29 12:43:39 +0800

    gameSearching + node server
    Merged the node server into this.

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const session = require("express-session");
-const app = express();
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on("connection", (socket) => {
+  console.log(`Server has connnected to id: ${socket.id}`)
+  socket.on("loggedIn", (username, uuid) => {
+      console.log([username, uuid])
+  });
+
+  socket.on("checkPassword", (gameUUID, userPasswordInput) => {
+    console.log(`gameUUID: ${gameUUID}`)
+    const gamePassword = "idfk"
+    console.log(userPasswordInput, gamePassword)
+    var success = false
+    if (userPasswordInput == gamePassword) {
+      success = true
+    }
+    socket.emit("passwordConfirmation", success)
+  })
+
+  socket.on("createNewGame", (options) => {});
+
+  socket.on("joinNewGame", (room) => {
+      socket.join(room);
+  });
+
+  socket.on("LeaveGame", () => {});
+
+  socket.on("RemoveGame", () => {});
+});
 
 app.set("view engine", "ejs");
 app.use(
@@ -11,23 +41,16 @@ app.use(
     saveUninitialized: true,
   })
 );
+
 app.use(bodyParser.urlencoded({ extended: true })); //add this
 app.use(bodyParser.json());
 app.use(sessionChecker);
 app.use(urlLogger)
 app.use(express.static('views'))
 
-const loginRouter = require("./routes/login");
-
-app.use("/", loginRouter);
-
-const userRouter = require("./routes/user");
-
-app.use("/user", userRouter);
-
-const gameRouter = require("./routes/game");
-
-app.use("/game", gameRouter);
+app.use("/", require("./routes/login"));
+app.use("/user", require("./routes/user"));
+app.use("/game", require("./routes/game"));
 
 function sessionChecker(req, res, next) {
   if (req.originalUrl.startsWith("/user/new/") || req.originalUrl == "/") return next();
@@ -44,4 +67,4 @@ function urlLogger(req, res, next) {
     next();
 }
 
-app.listen(3000);
+server.listen(3000);
